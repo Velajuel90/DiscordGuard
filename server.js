f7bf1a292b8bea25d14f5a4d63a0434b3e08e52b
Velajuel90 <41304177+Velajuel90@users.noreply.github.com>
2018-08-20 20:22:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1208,6 +1208,11 @@ if (message.content.startsWith("DG-logstest")) {
 }
 });
 
+client.on('guildMemberAdd', (member) => {
+ const Logs = client.channels.find('name', "Logs");
+  Logs.send(`${member.user.username} has joined the server`);
+}
+}):
 
 client.login(process.env.BOT_TOKEN);
 
