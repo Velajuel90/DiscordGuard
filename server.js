a5055ee80e9bc7f7bedf2224160f2c359d116cb0
Velajuel90 <41304177+Velajuel90@users.noreply.github.com>
2019-11-26 21:54:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-module.exports = (args) => {
-const express = require('./express.js');
-express(args);
-    };
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
